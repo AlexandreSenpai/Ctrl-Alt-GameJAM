24a2dc2a357487f3b59a246815148ce7c59ee9f8
Guilherme Simonato <guilhermesimonato4@gmail.com>
2024-02-10 16:20:17 -0300

    feat: add dialog system

---
+++ Assets/Scenes/_Scripts/Dialog.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogEntry 
+{
+    public string characterName;
+    [TextArea] public string dialogText;
+}
+
+public class Dialog : MonoBehaviour 
+{
+    [SerializeField] DialogManager dialogManager;
+    [SerializeField] List<DialogEntry> dialogEntries;
+
+    void Start() {
+        StartSpeech();
+    }
+
+    public void StartSpeech() 
+    {
+        // Chama o evento principal do dialog manager para iniciar o evento de conversa
+        dialogManager.InitiateDialog(dialogEntries);
+    }
+}
